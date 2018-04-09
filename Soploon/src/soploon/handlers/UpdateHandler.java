845980acc9ae1a594e342148da63e28f6ee585af
svallejos <sebastian.vallejos+github@isistan.unicen.edu.ar>
2018-04-09 11:09:06 -0300

    Gitignore y libs

--- Soploon/src/soploon/handlers/UpdateHandler.java
+++ Soploon/src/soploon/handlers/UpdateHandler.java
@@ -26,54 +26,63 @@ import soploon.teacher.Teacher;
 public class UpdateHandler extends AbstractHandler {
 
 	public UpdateHandler() {
-		
+
 	}
 
 	@Override
 	public Object execute(ExecutionEvent event) {
-		String rulesxml = getURL("http://si.isistan.unicen.edu.ar:8080/Rules/rules.xml");
-		if (rulesxml == null && event.getTrigger() != null) {
-			MessageDialog.openInformation(null, "Teaching Assistant", "No se pudo actualizar el Ayudante Virtual (servidor inalcanzable)");
+		try {
+			String rulesxml = getURL("http://si.isistan.unicen.edu.ar:8080/Rules/rules.xml");
+			if (rulesxml == null && event.getTrigger() != null) {
+				MessageDialog.openInformation(null, "Teaching Assistant",
+						"No se pudo actualizar el Ayudante Virtual (servidor inalcanzable)");
+				return null;
+			}
+			XStream xstream = new XStream();
+			xstream.processAnnotations(RuleSet.class);
+			RuleSet remote_rules = (RuleSet) xstream.fromXML(rulesxml);
+
+			PrologAnalyzer analyzer = Teacher.getInstance().getAnalyzer();
+			RuleSet local_rules = analyzer.getRules();
+			if (local_rules == null || !local_rules.getVersion().equals(remote_rules.getVersion())) {
+				String auxiliary = getURL("http://si.isistan.unicen.edu.ar:8080/Rules/auxiliary_predicates.pl");
+				String rules = getURL("http://si.isistan.unicen.edu.ar:8080/Rules/rules.pl");
+
+				if ((auxiliary == null || rules == null)) {
+					if (event.getTrigger() != null)
+						MessageDialog.openInformation(null, "Ayudante Virtual",
+								"No se pudo actualizar el Ayudante Virtual (servidor inalcanzable)");
+				} else {
+					if (analyzer.setRules(remote_rules, auxiliary, rules)) {
+						File dir = new File(PrologAnalyzer.BASE_PATH);
+						if (!dir.exists())
+							dir.mkdirs();
+						saveToFile(rulesxml, PrologAnalyzer.RULES_PATH);
+						saveToFile(auxiliary, PrologAnalyzer.AUXILIARY_PREDICATES_PATH);
+						saveToFile(rules, PrologAnalyzer.RULES_PATH);
+						if (event.getTrigger() != null)
+							MessageDialog.openInformation(null, "Ayudante Virtual", "Ayudante actualizado!");
+					} else if (event.getTrigger() != null) {
+						MessageDialog.openInformation(null, "Ayudante Virtual",
+								"No se pudo actualizar el Ayudante Virtual (error en la actualización)");
+					}
+				}
+			} else if (event.getTrigger() != null) {
+				MessageDialog.openInformation(null, "Ayudante Virtual",
+						"Usted ya dispone de la última version del Ayudante!");
+			}
+
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
 			return null;
 		}
-		XStream xstream = new XStream();
-		xstream.processAnnotations(RuleSet.class);
-		RuleSet remote_rules = (RuleSet) xstream.fromXML(rulesxml);
-
-		PrologAnalyzer analyzer = Teacher.getInstance().getAnalyzer();
-		RuleSet local_rules = analyzer.getRules();
-		if (local_rules == null || !local_rules.getVersion().equals(remote_rules.getVersion())) {
-			String auxiliary = getURL("http://si.isistan.unicen.edu.ar:8080/Rules/auxiliary_predicates.pl");
-			String rules = getURL("http://si.isistan.unicen.edu.ar:8080/Rules/rules.pl");
-
-			if ((auxiliary == null || rules == null)) {
-				if (event.getTrigger() != null)
-					MessageDialog.openInformation(null, "Ayudante Virtual", "No se pudo actualizar el Ayudante Virtual (servidor inalcanzable)");
-			} else {
-				if (analyzer.setRules(remote_rules, auxiliary, rules)) {
-					File dir = new File(PrologAnalyzer.BASE_PATH);
-					if (!dir.exists())
-						dir.mkdirs();
-					saveToFile(rulesxml, PrologAnalyzer.RULES_PATH);
-					saveToFile(auxiliary, PrologAnalyzer.AUXILIARY_PREDICATES_PATH);
-					saveToFile(rules, PrologAnalyzer.RULES_PATH);
-					if (event.getTrigger() != null)
-						MessageDialog.openInformation(null, "Ayudante Virtual", "Ayudante actualizado!");
-				} else if (event.getTrigger() != null) {		
-					MessageDialog.openInformation(null, "Ayudante Virtual", "No se pudo actualizar el Ayudante Virtual (error en la actualización)");
-				}
-			}
-		} else if (event.getTrigger() != null) {
-				MessageDialog.openInformation(null, "Ayudante Virtual", "Usted ya dispone de la última version del Ayudante!");
-		}
-
-		return null;
 	}
 
 	private String getURL(String path) {
 		try {
-			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(1 * 1000).build();
-			HttpClient http_client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();			
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(2000).build();
+			HttpClient http_client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
 			HttpGet get = new HttpGet(path);
 			HttpResponse response = http_client.execute(get);
 			HttpEntity entity = response.getEntity();
