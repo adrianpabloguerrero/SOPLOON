cf676edff6aa646a519288183e308787824bf5f7
Adrian Guerrero <43442350+adrianpabloguerrero@users.noreply.github.com>
2020-02-08 17:49:40 -0300

    Revert "Merge branch 'develop' of https://github.com/svallejos/Soploon into develop"
    This reverts commit 955becd7a35fd4d6265924c7cf3059559d14744f, reversing
    changes made to bd575799ddf1d201c7c65e4b96ae164f8ff709c2.

--- Private Version/FrontEnd/ant/src/components/Explorer.js
+++ Private Version/FrontEnd/ant/src/components/Explorer.js
@@ -9,19 +9,8 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import TreeView from "@material-ui/lab/TreeView";
-import TreeItem from "@material-ui/lab/TreeItem";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import Link from '@material-ui/core/Link';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import PackageIcon from '@material-ui/icons/BorderAll';
-import ExpandIcon from '@material-ui/icons/Add';
-import CollapseIcon from '@material-ui/icons/Remove';
-import FileIcon from '@material-ui/icons/Description';
 
 import Axios from 'axios';
 
@@ -34,37 +23,18 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
-  label: {
-    fontWeight: 'inherit',
-    color: 'inherit',
-  },
-  labelRoot: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0.5, 0),
-  },
-  labelIcon: {
-    marginRight: theme.spacing(1),
-	fontSize: 'small',
-  },
-  labelText: {
-    fontWeight: 'inherit',
-    flexGrow: 1,
-  }
 }));
 
 export default function Explorer() {
 
 	const classes = useStyles();
 	
-	const [state,setState] = React.useState({ user: null, project: null, correction: null});
-
 	const [users, setUsers] = React.useState([]);
 	const [projects, setProjects] = React.useState([]);
 	const [corrections, setCorrections] = React.useState([]);
-	const [sources, setSources] = React.useState([]);
-	const [sourceFile, setSourceFile] = React.useState({});
 
+	const [state,setState] = React.useState({ user: null, project: null, correction: null});
+	
 	const structureUsers = () => {
 		return users.map(user => (
 		  <ListItem key={user.id} onClick={ () => selectUser(user) } button> <ListItemText primary={user.name} /> </ListItem>
@@ -79,7 +49,7 @@ export default function Explorer() {
 	
 	const structureCorrections = () => {
 		return corrections.map(correction => (
-		  <ListItem key={correction.date} onClick={ () => selectCorrection(correction) } button> <ListItemText primary={ new Date(correction.date).toLocaleString() } /> </ListItem>
+		  <ListItem key={correction.date} button> <ListItemText primary={ new Date(correction.date).toLocaleString() } /> </ListItem>
 		));
 	};
 	
@@ -96,8 +66,7 @@ export default function Explorer() {
 	}
 	
 	const selectCorrection = (correction) => {
-		setState({user: state.user, project: state.project, correction: correction});
-		structureSource(correction);
+		setState({user: state.user, project: project, correction: correction});
 	}
 	
 	const showUsers = () => {
@@ -109,11 +78,7 @@ export default function Explorer() {
 	};
 	
 	const showCorrections = () => {
-		return (state.project != null && state.correction == null);
-	};
-	
-	const showSource = () => {
-		return (state.correction != null);
+		return (state.user != null && state.project != null && state.correction == null);
 	};
 	
 	const userName = () => {
@@ -168,109 +133,6 @@ export default function Explorer() {
 			return null;
 	}
 	
-	const correctionName = () => {
-		return state.correction != null ? new Date(state.correction.date).toLocaleString() : '';
-	};
-
-	const getCorrectionNavigation = () =>  {
-		if (state.correction != null)			
-			return (<Link color="inherit"> { correctionName() } </Link>)
-		else 
-			return null;
-	}
-		
-	const structureSource = (correction) => {
-		let folders = {childrens: []};
-		
-		correction.code.forEach(sourceFile => {
-			var paths = sourceFile.path.split("/");
-			paths = paths.slice(1,paths.length);
-			
-			var partialPath = "";
-			
-			var currentFolder = folders;
-			paths.forEach(path => {
-				var current = null;
-				partialPath = partialPath + "/" + path;
-				currentFolder.childrens.forEach(node => {
-					if (node.name === path)
-						current = node;
-				});
-				
-				if (current == null) {
-					current = {};
-					current.name = path;
-					current.id = partialPath;
-					current.childrens = [];
-					currentFolder.childrens.push(current);
-				}
-
-				currentFolder = current;
-				
-			});
-			
-			currentFolder.source = sourceFile.code;
-	
-		});
-		
-		setSources(folders.childrens);
-	}
-	
-	const showSourceFile = () => {
-		return sourceFile.name != undefined;
-	}
-	
-	const getTreeItemsFromData = treeItems => {
-	  return treeItems.map(treeItemData => {
-		let children = undefined;
-		if (treeItemData.childrens && treeItemData.childrens.length > 0) {
-		  children = getTreeItemsFromData(treeItemData.childrens);
-		}
-		if (treeItemData.childrens.length > 0) {
-			return (
-			  <TreeItem
-				key={treeItemData.id}
-				nodeId={treeItemData.id}
-				label={
-					<div className={classes.labelRoot}>
-					  <PackageIcon color="inherit" className={classes.labelIcon} />
-					  <Typography variant="body2" className={classes.labelText}>
-						{treeItemData.name}
-					  </Typography>
-					</div>
-				}
-				children={children}
-			  />
-			);
-		} else {
-			return (
-			  <TreeItem onClick={ () => { setSourceFile(treeItemData); } }
-				key={treeItemData.id}
-				nodeId={treeItemData.id}
-				label={
-					<div className={classes.labelRoot}>
-					  <FileIcon color="inherit" className={classes.labelIcon} />
-					  <Typography variant="body2" className={classes.labelText}>
-						{treeItemData.name}
-					  </Typography>
-					</div>
-				}
-				children={children}
-			/>)
-		}
-	  });
-	};
-	
-	const DataTreeView = ({ treeItems }) => {
-	  return (
-		<TreeView
-		  defaultExpandIcon={<ExpandIcon  className={classes.labelIcon}/>}
-		  defaultCollapseIcon={<CollapseIcon  className={classes.labelIcon}/>}>
-		  {getTreeItemsFromData(treeItems)}
-		</TreeView>
-	  );
-	};
-
 	useEffect(() => { getUsers(); }, []);
 
 	return (
@@ -279,38 +141,25 @@ export default function Explorer() {
 				<Grid item xs={12}>
 					<Paper className={classes.paper}>
 					  <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-					  	<Link color="inherit" onClick={() => selectUser(null)}> Inicio </Link>
+					  	<Link color="inherit" onClick={ () => selectUser(null)}> Inicio </Link>
 						{getUserNavigation()}
 						{getProjectNavigation()}
-						{getCorrectionNavigation()}
 					  </Breadcrumbs>
 					</Paper>
 				</Grid>
 				<Grid item xs={3}>
 					<Paper className={classes.paper}>
 						<List hidden={!showUsers()} subheader={ <div><ListSubheader component="div"> Usuarios </ListSubheader></div> }>
-							{structureUsers()}
+							{structureUsers(users)}
 						</List>
 						<List hidden={!showProjects()} subheader={<div> <ListSubheader component="div"> { "Projectos de " + userName() } </ListSubheader></div>}>
-							{structureProjects()}
+							{structureProjects(projects)}
 						</List>
 						<List hidden={!showCorrections()} subheader={<div> <ListSubheader component="div"> { "Correcciones de " + projectName() } </ListSubheader></div>}>
-							{structureCorrections()}
+							{structureCorrections(projects)}
 						</List>
-						<div hidden={!showSource()} style={{'textAlign': 'left'}}>
-							<DataTreeView treeItems={sources} />
-						</div>
 					</Paper>
-				</Grid>
-				<Grid hidden={!showSourceFile()} item xs={9}>
-					<Paper className={classes.paper}>
-						<div style={{'textAlign': 'left'}}>
-							{ sourceFile.name }
-							<SyntaxHighlighter language="java" style={docco}>
-							  { sourceFile.source != undefined ? sourceFile.source.replace(/\t/g,"   ") : ''}
-							</SyntaxHighlighter>
-						</div>
-					</Paper>
+
 				</Grid>
 			</Grid>
 		</div>
