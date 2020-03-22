9cbfe428960341ea085d6a389f5aa009813d6405
Adrian Guerrero <43442350+adrianpabloguerrero@users.noreply.github.com>
2020-03-21 21:36:01 -0300

    No warnings

--- Private Version/FrontEnd/ant/src/components/errors/Filter.js
+++ Private Version/FrontEnd/ant/src/components/errors/Filter.js
@@ -3,7 +3,6 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from "@material-ui/core/TextField";
 import InputLabel from '@material-ui/core/InputLabel';
-import Grid from '@material-ui/core/Grid';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import TableFilter from './TableFilter';
--- Private Version/FrontEnd/ant/src/components/explorer/explorer-utils.js
+++ Private Version/FrontEnd/ant/src/components/explorer/explorer-utils.js
@@ -2,16 +2,16 @@
 const ExplorerUtils = {};
 
 ExplorerUtils.structureCorrection = (correction) => {
-	
+
 	if (correction != null) {
 		let folders = {childrens: []};
 
 		correction.code.forEach(sourceFile => {
 			var paths = sourceFile.path.split("/");
 			paths = paths.slice(1,paths.length);
-			
+
 			var partialPath = "";
-			
+
 			var currentFolder = folders;
 			paths.forEach(path => {
 				var current = null;
@@ -20,7 +20,7 @@ ExplorerUtils.structureCorrection = (correction) => {
 					if (node.name === path)
 						current = node;
 				});
-				
+
 				if (current == null) {
 					current = {};
 					current.name = path;
@@ -30,37 +30,37 @@ ExplorerUtils.structureCorrection = (correction) => {
 				}
 
 				currentFolder = current;
-				
+
 			});
-			
+
 			currentFolder.source = sourceFile.code;
 
 		});
-		
+
 		return folders.childrens;
-	
+
 	}
-	
+
 }
 
 ExplorerUtils.simplifiedStructureCorrection = (correction) => {
-	
+
 	if (correction !== null) {
 
 		var folders = ExplorerUtils.structureCorrection(correction);
-		
-		var toAnalyze = new Array();
+
+		var toAnalyze = [];
 
 		folders.forEach(folder => {
 			toAnalyze.push(folder);
 		});
 
-		
-		for (var index = 0; index < toAnalyze.length; index++) { 
+
+		for (var index = 0; index < toAnalyze.length; index++) {
 			var folder = toAnalyze[index];
-				
+
 			if (folder.source === undefined) {
-				if (folder.childrens.length == 1) {
+				if (folder.childrens.length === 1) {
 					var children = folder.childrens[0];
 					folder.id = children.id;
 					folder.childrens = children.childrens;
@@ -76,11 +76,11 @@ ExplorerUtils.simplifiedStructureCorrection = (correction) => {
 				index++;
 			}
 		}
-				
+
 		return folders;
-		
+
 	}
-	
+
 }
 
 
