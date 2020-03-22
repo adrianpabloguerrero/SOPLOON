957333334a6f87d997a21dab95b99edf5cedaa42
Adrian Guerrero <43442350+adrianpabloguerrero@users.noreply.github.com>
2020-03-21 22:17:13 -0300

    Format

--- Private Version/FrontEnd/ant/src/components/errors/Errors.js
+++ Private Version/FrontEnd/ant/src/components/errors/Errors.js
@@ -1,53 +1,53 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import PieChartIcon from '@material-ui/icons/PieChart';
-import StorageIcon from '@material-ui/icons/Storage';
-import TableErrors from "./TableErrors";
-import PieChart from "./PieChart";
-import BarChart from "./BarChart";
-import Filter from "./Filter";
-import Style from '../utils/style';
+import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import BarChartIcon from '@material-ui/icons/BarChart'
+import PieChartIcon from '@material-ui/icons/PieChart'
+import StorageIcon from '@material-ui/icons/Storage'
+import TableErrors from './TableErrors'
+import PieChart from './PieChart'
+import BarChart from './BarChart'
+import Filter from './Filter'
+import Style from '../utils/style'
 
-export default function Errors() {
+export default function Errors () {
+  const style = Style()
+  const [valueNavBar, setValueNavBar] = React.useState(0)
+  const [errorsSelected, setErrorsSelected] = React.useState([])
 
-const [valueNavBar, setValueNavBar] = React.useState(0);
-const style = Style();
-
- //Errores visibles
-const [errorsSelected, setErrorsSelected] = React.useState([]);
-
-    return (
-      <div className={style.root} >
-      <Grid container alignItems="stretch"spacing={3}>
+  return (
+    <div className={style.root} >
+      <Grid container alignItems='stretch'spacing={3}>
         <Grid item xs={4}>
-          <Filter style={style} handleErrorsSelected={setErrorsSelected}/>
+          <Filter style={style} handleErrorsSelected={setErrorsSelected} />
         </Grid>
         <Grid item xs={8} className={style.container}>
-              <BottomNavigation style = {{marginBottom: "18px"}}
-              showLabels
-              value={valueNavBar}
-              onChange={(event, newValue) => {
-              setValueNavBar(newValue);
+          <BottomNavigation style={{marginBottom: '18px'}}
+            showLabels
+            value={valueNavBar}
+            onChange={(event, newValue) => {
+              setValueNavBar(newValue)
             }}
-
               >
-              <BottomNavigationAction label="Lista" icon={<StorageIcon />} />
-              <BottomNavigationAction label="Pie Chart" icon={<PieChartIcon />} />
-              <BottomNavigationAction label="Bar Chart" icon={<BarChartIcon />}  />
-              </BottomNavigation>
-        { (valueNavBar === 0) && (<TableErrors errors={errorsSelected} setErrorsSelected={setErrorsSelected}/>) }
-        { (valueNavBar === 1) && (<Paper className={style.paperInfo}>
-          <PieChart errors={errorsSelected} />
-        </Paper>) }
-        { (valueNavBar === 2) && (<Paper className={style.paperInfo}>
-          <BarChart errors={errorsSelected}/>
-          </Paper>) }
+            <BottomNavigationAction label='Lista' icon={<StorageIcon />} />
+            <BottomNavigationAction label='Pie Chart' icon={<PieChartIcon />} />
+            <BottomNavigationAction label='Bar Chart' icon={<BarChartIcon />} />
+          </BottomNavigation>
+          { (valueNavBar === 0) &&
+            (<TableErrors errors={errorsSelected} setErrorsSelected={setErrorsSelected} />) }
+          { (valueNavBar === 1) &&
+          (<Paper className={style.paperInfo}>
+            <PieChart errors={errorsSelected} />
+          </Paper>)
+          }
+          { (valueNavBar === 2) && (<Paper className={style.paperInfo}>
+            <BarChart errors={errorsSelected} />
+            </Paper>)
+          }
         </Grid>
       </Grid>
     </div>
-    );
+  )
 }
--- Private Version/FrontEnd/ant/src/components/errors/TableErrors.js
+++ Private Version/FrontEnd/ant/src/components/errors/TableErrors.js
@@ -1,40 +1,37 @@
-import React from 'react';
-import MaterialTable from 'material-table';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import Axios from 'axios';
+import React from 'react'
+import MaterialTable from 'material-table'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import Axios from 'axios'
 
 let nameClasses = (error) => {
-  let out = "";
+  let out = ''
   error.codeLocation.forEach((item, i) => {
-    if (out.length > 0){
-      out += ", ";
-   }
-    let aux = item.path.substring(item.path.lastIndexOf("/")+1,item.path.length).substring();
-    out += aux.substring(0,aux.indexOf("."));
-  });
-  return out;
+    if (out.length > 0) {
+      out += ', '
+    }
+    let aux = item.path.substring(item.path.lastIndexOf('/') + 1, item.path.length).substring()
+    out += aux.substring(0, aux.indexOf('.'))
+  })
+  return out
 }
 
-
 export default function TableErrors ({ errors, setErrorsSelected }) {
-
-  const handleReviewedChange = (e,error) => {
-  
-    //Actualizo la tabla
-    errors[errors.indexOf(error)].reviewed = e.target.value;
-    let aux = [...errors];
-    setErrorsSelected(aux);
+  const handleReviewedChange = (e, error) => {
+    // Actualizo la tabla
+    errors[errors.indexOf(error)].reviewed = e.target.value
+    let aux = [...errors]
+    setErrorsSelected(aux)
 
     const path = {
-      id:error.id,
+      id: error.id,
       userId: error.userId,
       projectId: error.projectId,
       date: error.date
     }
 
     const modifiedError = {
-      id:error.id,
+      id: error.id,
       userId: error.userId,
       projectId: error.projectId,
       date: error.date,
@@ -45,66 +42,66 @@ export default function TableErrors ({ errors, setErrorsSelected }) {
       reviewed: e.target.value
     }
 
-    let url = 'http://localhost:8080/soploon/api/users/'+path.userId+'/projects/'+path.projectId+'/corrections/'+path.date+'/errors/'+path.id;
+    let url = 'http://localhost:8080/soploon/api/users/' + path.userId + '/projects/' + path.projectId + '/corrections/' + path.date + '/errors/' + path.id
     Axios.put(url, modifiedError)
       .then(response => {
       })
-      .catch(response => {console.log(response);});
-   }
+      .catch(response => { console.log(response) })
+  }
 
   return (
     <MaterialTable
-    localization={{
-           pagination: {
-               labelRowsPerPage:'Filas por página',
-               labelRowsSelect: 'Filas',
-               firstAriaLabel: 'Primera página',
-               firstTooltip:'Primera página',
-               previousAriaLabel: 'Página anterior',
-               previousTooltip: 'Página anterior',
-               nextAriaLabel: 'Próxima página',
-               nextTooltip: 'Próxima página',
-               lastAriaLabel: 'Última página',
-               lastTooltip: 'Última página'
-           },
-           toolbar: {
-               nRowsSelected: '{0} filas(s) seleccionadas',
-               searchTooltip: 'Buscar',
-               searchPlaceholder: 'Buscar'
-           },
-           header: {
-               actions: 'Acciones'
-           },
-           body: {
-               emptyDataSourceMessage: 'No hay datos para mostrar',
+      localization={{
+        pagination: {
+          labelRowsPerPage: 'Filas por página',
+          labelRowsSelect: 'Filas',
+          firstAriaLabel: 'Primera página',
+          firstTooltip: 'Primera página',
+          previousAriaLabel: 'Página anterior',
+          previousTooltip: 'Página anterior',
+          nextAriaLabel: 'Próxima página',
+          nextTooltip: 'Próxima página',
+          lastAriaLabel: 'Última página',
+          lastTooltip: 'Última página'
+        },
+        toolbar: {
+          nRowsSelected: '{0} filas(s) seleccionadas',
+          searchTooltip: 'Buscar',
+          searchPlaceholder: 'Buscar'
+        },
+        header: {
+          actions: 'Acciones'
+        },
+        body: {
+          emptyDataSourceMessage: 'No hay datos para mostrar',
 
-               editRow: {
-                 saveTooltip: 'Guardar',
-                 cancelTooltip: 'Cancelar',
-                 deleteText: '¿Está seguro que lo desea eliminar?'
-               }
-           }
-       }}
-       actions={[
-          {
-            icon: 'search',
-            tooltip: 'Ver en el explorador',
-            onClick: () => console.log("ver error")
+          editRow: {
+            saveTooltip: 'Guardar',
+            cancelTooltip: 'Cancelar',
+            deleteText: '¿Está seguro que lo desea eliminar?'
           }
-        ]}
-      title="Errores"
+        }
+      }}
+      actions={[
+        {
+          icon: 'search',
+          tooltip: 'Ver en el explorador',
+          onClick: () => console.log('ver error')
+        }
+      ]}
+      title='Errores'
       columns={[
                  { title: 'Usuario', field: 'nameUser' },
                  { title: 'Regla', field: 'nameRule' },
                  { title: 'Proyecto', field: 'nameProject' },
-                 { title: 'Clase', field:'nameclass', render: error => nameClasses(error)},
-                 { title: 'Estado', field: 'reviewed', render: error =>
-                 <Select labelId="label" name="reviewed" id="select" onChange={(event) => handleReviewedChange(event,error)} value={error.reviewed}>
-                      <MenuItem key="0" value={0}>No revisado</MenuItem>
-                      <MenuItem key="1" value={1}>Falso positivo</MenuItem>
-                      <MenuItem key="2" value={2}>Confirmado</MenuItem>
-                 </Select>
-                 }
-              ]}
+                 { title: 'Clase', field: 'nameclass', render: error => nameClasses(error)},
+        { title: 'Estado', field: 'reviewed', render: error =>
+          <Select labelId='label' name='reviewed' id='select' onChange={(event) => handleReviewedChange(event, error)} value={error.reviewed}>
+            <MenuItem key='0' value={0}>No revisado</MenuItem>
+            <MenuItem key='1' value={1}>Falso positivo</MenuItem>
+            <MenuItem key='2' value={2}>Confirmado</MenuItem>
+          </Select>
+        }
+      ]}
       data={errors} />)
 }
