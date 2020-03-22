935a65291f1ef74f3d88b6b5adf55ea0288066b4
Adrian Guerrero <43442350+adrianpabloguerrero@users.noreply.github.com>
2020-03-22 16:59:04 -0300

    Se reubican archivos en carpetas

--- Private Version/FrontEnd/ant/src/components/users/Users.js
+++ Private Version/FrontEnd/ant/src/components/users/Users.js
@@ -47,8 +47,6 @@ export default function MaterialTableDemo() {
 
   return (
     <MaterialTable
-
-
     localization={{
            pagination: {
                labelDisplayedRows: '{from}-{to} de {count}',
